302e071546a26832fe2a80006f493e4fdf6c6a82
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 02:23:49 +0500

    added brute force twoSum

--- hashtable/twoSum/brute/twoSum.java
+++ hashtable/twoSum/brute/twoSum.java
@@ -1,5 +1,30 @@
 package hashtable.twoSum.brute;
 
-public class twoSum {
-    
+class twoSum {
+    public static int[] twoSumF(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[]{};
+    }
+
+
+
+    public static void main(String[] args) {
+
+      int[] i = {2,7,11,15};
+
+      int[] result = twoSumF(i, 9);
+
+      for(int o: result){
+          System.out.print(o +", ");
+      }
+    }
 }
