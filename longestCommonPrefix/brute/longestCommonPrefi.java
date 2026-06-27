99fed1b601c47a0dafdcc7d80175be2eabfaa410
Abdullah Islam <itsabddev@gmail.com>
2026-06-27 16:35:21 +0500

    Added brute force longestCommonPrefix

---
+++ longestCommonPrefix/brute/longestCommonPrefi.java
@@ -0,0 +1,29 @@
+package longestCommonPrefix.brute;
+
+class longestCommonPrefix {
+    public static String longestCommonPrefixFunc(String[] strs) {
+
+        for (int i = 0; i < strs[0].length(); i++) {
+
+            char c = strs[0].charAt(i);
+
+            for (int j = 1; j < strs.length; j++) {
+
+                if (i >= strs[j].length()||strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+
+        return strs[0];
+    };
+
+    public static void main(String[] args) {
+
+      String[] strs= {"flower","flow","flight"};
+
+      String result = longestCommonPrefixFunc(strs);
+
+        System.out.println(result);
+    }
+}
