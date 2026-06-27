1b66830607a2257173b53c85821a1723ef8aa448
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 02:54:17 +0500

    added optimal MaximimBalloons

--- hashtable/maximumNumberOfBalloons/optimal/maximumNumberOfBalloons.java
+++ hashtable/maximumNumberOfBalloons/optimal/maximumNumberOfBalloons.java
@@ -1,5 +1,26 @@
 package hashtable.maximumNumberOfBalloons.optimal;
 
-public class maximumNumberOfBalloons {
-    
+class maximumNumberOfBalloons{
+    public static int maxNumberOfBalloonsF(String text) {
+        int[] freq = new int[26];
+        for (char c : text.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        int b = freq['b' - 'a'];
+        int a = freq['a' - 'a'];
+        int l = freq['l' - 'a'] / 2;
+        int o = freq['o' - 'a'] / 2;
+        int n = freq['n' - 'a'];
+
+        return Math.min(Math.min(b, a), Math.min(Math.min(l, o), n));
+    }
+
+    public static void main(String[] args) {
+
+
+      int result = maxNumberOfBalloonsF("balloonnoollab");
+
+        System.out.println(result);
+    }
 }
