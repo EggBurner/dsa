fec1f4e3f091701b13eca43f95908d256fc12fb6
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 02:23:49 +0500

    added optimal twoSum

--- hashtable/twoSum/optimal/twoSum.java
+++ hashtable/twoSum/optimal/twoSum.java
@@ -1,5 +1,32 @@
 package hashtable.twoSum.optimal;
 
-public class twoSum {
-    
+import java.util.HashMap;
+
+class twoSum {
+    public static int[] twoSumF(int[] nums, int target) {
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 0; i <nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+
+    public static void main(String[] args) {
+
+      int[] i = {2,7,11,15};
+
+      int[] result = twoSumF(i, 9);
+
+      for(int o: result){
+          System.out.print(o +", ");
+      }
+    }
 }
