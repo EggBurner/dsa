2afd1d4894a2d20708d4b0818145cf8003ae5934
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 02:17:01 +0500

    added optimal containsDuplicate

--- hashtable/containsDuplicate/optimal/containsDuplicate.java
+++ hashtable/containsDuplicate/optimal/containsDuplicate.java
@@ -1,5 +1,29 @@
 package hashtable.containsDuplicate.optimal;
 
-public class containsDuplicate {
-    
+import java.util.HashSet;
+
+class containsDuplicate {
+    public static boolean containsDuplicateF(int[] nums) {
+
+        HashSet<Integer> mySet = new HashSet<>();
+
+        for (int num : nums) {
+            if (mySet.contains(num)) {
+                return true;
+            }
+            mySet.add(num);
+        }
+        return false;
+    }
+
+
+
+    public static void main(String[] args) {
+
+      int[] i = {7,1,5,3,6,4,1};
+
+      boolean result = containsDuplicateF(i);
+
+        System.out.println(result);
+    }
 }
