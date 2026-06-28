23be739fc6cc1285adbc0ff31d236431ded547b0
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 14:09:09 +0500

    added optimal containerWithMostWater

--- twoPointer/containerWithMostWater/optimal/containerWithMostWater.java
+++ twoPointer/containerWithMostWater/optimal/containerWithMostWater.java
@@ -1,5 +1,34 @@
 package twoPointer.containerWithMostWater.optimal;
 
-public class containerWithMostWater {
-    
+class containerWithMostWater {
+    public static int containerWithMostWaterF(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+        int max = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int h = Math.min(height[left], height[right]);
+
+            int area = width * h;
+            max = Math.max(max, area);
+            
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] i = {1,8,6,2,5,4,8,3,7};
+        int result = containerWithMostWaterF(i);
+        System.out.println(result);
+
+    }
 }
+
