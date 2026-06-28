ed954209faa1c0a3017649871bad1c8410e41b73
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 16:39:03 +0500

    added brute force variableBst

--- bst/variableBst/brute/variableBst.java
+++ bst/variableBst/brute/variableBst.java
@@ -1,5 +1,31 @@
 package bst.variableBst.brute;
 
-public class variableBst {
-    
+import java.util.ArrayList;
+
+import bst.TreeNode;
+
+class ValidateBST {
+
+    public static boolean ValidateBSTF(TreeNode root) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        inorder(root, list);
+
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) <= list.get(i - 1)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static void inorder(TreeNode root, ArrayList<Integer> list) {
+
+        if (root == null) return;
+
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
 }
