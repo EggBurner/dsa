f25f1ecadb0475e5bd5d624eb01e814b50dd4aae
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 15:28:35 +0500

    added brute and optimal mergeSortedLists

--- linkedList/mergeTwoSortedLists/brute/mergeSortedLists.java
+++ linkedList/mergeTwoSortedLists/brute/mergeSortedLists.java
@@ -1,5 +1,35 @@
 package linkedList.mergeTwoSortedLists.brute;
 
-public class mergeSortedLists {
-    
+import java.util.*;
+
+import linkedList.ListNode;
+
+
+class mergeTwoLists {
+    public static ListNode mergeTwoListsF(ListNode list1, ListNode list2) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while (list1 != null) {
+            list.add(list1.val);
+            list1 = list1.next;
+        }
+
+        while (list2 != null) {
+            list.add(list2.val);
+            list2 = list2.next;
+        }
+
+        Collections.sort(list);
+
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        for (int val : list) {
+            curr.next = new ListNode(val);
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
 }
