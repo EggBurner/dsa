78ab2ff3ab90434b8b175d9a14b8283e8db1f106
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 14:05:34 +0500

    added bruteforce containerWithMostWater

--- twoPointer/containerWithMostWater/brute/containerWithMostWater.java
+++ twoPointer/containerWithMostWater/brute/containerWithMostWater.java
@@ -1,5 +1,29 @@
 package twoPointer.containerWithMostWater.brute;
 
-public class containerWithMostWater {
-    
+class containerWithMostWater {
+    public static int containerWithMostWaterF(int[] height) {
+
+        int max = 0;
+
+        for (int i = 0; i < height.length; i++) {
+
+            for (int j = i + 1; j < height.length; j++) {
+
+                int area = Math.min(height[i], height[j]) * (j - i);
+
+                if (area > max) {
+                    max = area;
+                }
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] i = {1,8,6,2,5,4,8,3,7};
+        int result = containerWithMostWaterF(i);
+        System.out.println(result);
+
+    }
 }
