26c34f51d7935b03d954442085058b6ddbe2de49
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 16:32:36 +0500

    added brute force maxDepth

--- bst/maxDepth/brute/maxDepth.java
+++ bst/maxDepth/brute/maxDepth.java
@@ -1,5 +1,36 @@
 package bst.maxDepth.brute;
 
-public class maxDepth {
-    
+import java.util.LinkedList;
+import java.util.Queue;
+
+import bst.TreeNode;
+
+class MaxDepth {
+
+    public static int MaxDepthF(TreeNode root) {
+
+        if (root == null) return 0;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        int depth = 0;
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+
+            while (size-- > 0) {
+
+                TreeNode node = queue.poll();
+
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+
+            depth++;
+        }
+
+        return depth;
+    }
 }
