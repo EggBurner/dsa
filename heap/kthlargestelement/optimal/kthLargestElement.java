cfeb108c32b58257a18e55748c1b61ce9165902d
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 16:11:20 +0500

    added optimal kth largest Element

--- heap/kthlargestelement/optimal/kthLargestElement.java
+++ heap/kthlargestelement/optimal/kthLargestElement.java
@@ -1,5 +1,17 @@
 package heap.kthlargestelement.optimal;
 
-public class kthLargestElement {
-    
+import java.util.*;
+
+class KthLargestElement {
+    public static int KthLargestElementF(int[] nums, int k) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int x : nums) {
+            pq.add(x);
+            if (pq.size() > k) pq.poll();
+        }
+
+        return pq.peek();
+    }
 }
