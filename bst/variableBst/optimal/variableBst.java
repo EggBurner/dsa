e0a817e6159e448abe325759b8b480f565f30416
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 16:39:53 +0500

    added optimal variableBst

--- bst/variableBst/optimal/variableBst.java
+++ bst/variableBst/optimal/variableBst.java
@@ -1,5 +1,22 @@
 package bst.variableBst.optimal;
 
-public class variableBst {
-    
+import bst.TreeNode;
+
+class ValidateBST {
+
+    public static boolean ValidateBSTF(TreeNode root) {
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private static boolean helper(TreeNode root, long min, long max) {
+
+        if (root == null) return true;
+
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+
+        return helper(root.left, min, root.val) &&
+               helper(root.right, root.val, max);
+    }
 }
