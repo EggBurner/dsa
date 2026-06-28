63389deb163c83150630aebee3928e22bf56d2c5
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 16:42:04 +0500

    added optimal implementTrie

--- bst/implementTrie/optimal/implementTrie.java
+++ bst/implementTrie/optimal/implementTrie.java
@@ -1,5 +1,69 @@
 package bst.implementTrie.optimal;
 
-public class implementTrie {
-    
+class ImplementTrie {
+
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean end;
+    }
+
+    private TrieNode root;
+
+    public ImplementTrie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+
+        TrieNode curr = root;
+
+        for (char c : word.toCharArray()) {
+
+            int index = c - 'a';
+
+            if (curr.children[index] == null) {
+                curr.children[index] = new TrieNode();
+            }
+
+            curr = curr.children[index];
+        }
+
+        curr.end = true;
+    }
+
+    public boolean search(String word) {
+
+        TrieNode curr = root;
+
+        for (char c : word.toCharArray()) {
+
+            int index = c - 'a';
+
+            if (curr.children[index] == null) {
+                return false;
+            }
+
+            curr = curr.children[index];
+        }
+
+        return curr.end;
+    }
+
+    public boolean startsWith(String prefix) {
+
+        TrieNode curr = root;
+
+        for (char c : prefix.toCharArray()) {
+
+            int index = c - 'a';
+
+            if (curr.children[index] == null) {
+                return false;
+            }
+
+            curr = curr.children[index];
+        }
+
+        return true;
+    }
 }
