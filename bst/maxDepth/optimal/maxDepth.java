3616c986c62346d7eeccdaa22588c7e37c582298
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 16:33:28 +0500

    added optimal maxDepth

--- bst/maxDepth/optimal/maxDepth.java
+++ bst/maxDepth/optimal/maxDepth.java
@@ -1,5 +1,13 @@
 package bst.maxDepth.optimal;
 
-public class maxDepth {
-    
+import bst.TreeNode;
+
+class MaxDepth {
+
+    public static int MaxDepthF(TreeNode root) {
+
+        if (root == null) return 0;
+
+        return 1 + Math.max(MaxDepthF(root.left), MaxDepthF(root.right));
+    }
 }
