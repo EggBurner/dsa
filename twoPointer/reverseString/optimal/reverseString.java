741e0b592edd829297f8af562e50d9a7eb70df8b
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 13:42:29 +0500

    added optimal reverseString

--- twoPointer/reverseString/optimal/reverseString.java
+++ twoPointer/reverseString/optimal/reverseString.java
@@ -1,5 +1,21 @@
 package twoPointer.reverseString.optimal;
 
-public class reverseString {
-    
+class reverseString {
+    public static void reverseStringF(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        reverseStringF(new char[]{'a','b','d','u','l','l', 'a', 'h'});
+    }
 }
