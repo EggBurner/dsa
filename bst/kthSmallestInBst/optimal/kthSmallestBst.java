b2a67a1d565292235d9b317050ba609b9a60f6ec
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 16:35:23 +0500

    added optimal kthSmallestBst

--- bst/kthSmallestInBst/optimal/kthSmallestBst.java
+++ bst/kthSmallestInBst/optimal/kthSmallestBst.java
@@ -1,5 +1,29 @@
 package bst.kthSmallestInBst.optimal;
 
-public class kthSmallestBst {
-    
+import java.util.Stack;
+
+import bst.TreeNode;
+
+class KthSmallestInBST {
+
+    public static int KthSmallestInBSTF(TreeNode root, int k) {
+
+        Stack<TreeNode> stack = new Stack<>();
+
+        while (root != null || !stack.isEmpty()) {
+
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+
+            if (--k == 0) return root.val;
+
+            root = root.right;
+        }
+
+        return -1;
+    }
 }
