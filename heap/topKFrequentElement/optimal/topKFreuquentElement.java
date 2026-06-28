2536267bc11cb5d4ab81b8707d46cb083e625368
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 16:14:41 +0500

    added optimal topKFrequentElement

--- heap/topKFrequentElement/optimal/topKFreuquentElement.java
+++ heap/topKFrequentElement/optimal/topKFreuquentElement.java
@@ -1,5 +1,29 @@
 package heap.topKFrequentElement.optimal;
 
-public class topKFreuquentElement {
-    
+import java.util.*;
+
+class TopKFrequent {
+    public static int[] TopKFrequentF(int[] nums, int k) {
+
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        for (int x : nums) {
+            freq.put(x, freq.getOrDefault(x, 0) + 1);
+        }
+        PriorityQueue<Integer> pq =
+            new PriorityQueue<>((a, b) -> freq.get(a) - freq.get(b));
+
+        for (int key : freq.keySet()) {
+            pq.add(key);
+            if (pq.size() > k) pq.poll();
+        }
+
+        int[] res = new int[k];
+
+        for (int i = k - 1; i >= 0; i--) {
+            res[i] = pq.poll();
+        }
+
+        return res;
+    }
 }
