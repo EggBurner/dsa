967db885746e0574af6283a835fa3195e1260960
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 15:38:37 +0500

    added optimal linkedListCycle

--- linkedList/linkedListCycle/optimal/linkedListCycle.java
+++ linkedList/linkedListCycle/optimal/linkedListCycle.java
@@ -1,5 +1,24 @@
 package linkedList.linkedListCycle.optimal;
 
+import linkedList.ListNode;
+
 public class linkedListCycle {
-    
+    public boolean linkedListCycleF(ListNode head) {
+        if (head == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
