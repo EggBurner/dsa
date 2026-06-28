f46f7650dcd65aad89d5e6986e35be52ab3249a8
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 16:37:06 +0500

    added optimal diameter

--- bst/diameter/optimal/diameter.java
+++ bst/diameter/optimal/diameter.java
@@ -1,5 +1,27 @@
 package bst.diameter.optimal;
 
-public class diameter {
-    
+import bst.TreeNode;
+
+class DiameterOfBinaryTree {
+
+    private static int diameter;
+
+    public static int DiameterOfBinaryTreeF(TreeNode root) {
+
+        diameter = 0;
+        height(root);
+        return diameter;
+    }
+
+    private static int height(TreeNode root) {
+
+        if (root == null) return 0;
+
+        int left = height(root.left);
+        int right = height(root.right);
+
+        diameter = Math.max(diameter, left + right);
+
+        return 1 + Math.max(left, right);
+    }
 }
