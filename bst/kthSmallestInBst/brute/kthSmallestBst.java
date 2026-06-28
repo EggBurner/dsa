c1b490dd63d56cae1d8186435a45f04714525b8f
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 16:34:38 +0500

    added brute force kthSmallestBst

--- bst/kthSmallestInBst/brute/kthSmallestBst.java
+++ bst/kthSmallestInBst/brute/kthSmallestBst.java
@@ -1,5 +1,25 @@
 package bst.kthSmallestInBst.brute;
 
-public class kthSmallestBst {
-    
+import java.util.ArrayList;
+
+import bst.TreeNode;
+
+class KthSmallestInBST {
+
+    public static int KthSmallestInBSTF(TreeNode root, int k) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        inorder(root, list);
+
+        return list.get(k - 1);
+    }
+
+    private static void inorder(TreeNode root, ArrayList<Integer> list) {
+
+        if (root == null) return;
+
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
 }
