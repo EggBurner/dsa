26be09ac86540686b1bb97dcb8536d70f88bc1eb
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 16:44:39 +0500

    added optimal minAbsDiffInBst

--- bst/minAbsDiffInBst/optimal/minAbsDiffInBst.java
+++ bst/minAbsDiffInBst/optimal/minAbsDiffInBst.java
@@ -1,5 +1,34 @@
 package bst.minAbsDiffInBst.optimal;
 
-public class minAbsDiffInBst {
-    
+import bst.TreeNode;
+
+class MinAbsDiffInBST {
+
+    private static Integer prev;
+    private static int min;
+
+    public static int MinAbsDiffInBSTF(TreeNode root) {
+
+        prev = null;
+        min = Integer.MAX_VALUE;
+
+        inorder(root);
+
+        return min;
+    }
+
+    private static void inorder(TreeNode root) {
+
+        if (root == null) return;
+
+        inorder(root.left);
+
+        if (prev != null) {
+            min = Math.min(min, root.val - prev);
+        }
+
+        prev = root.val;
+
+        inorder(root.right);
+    }
 }
