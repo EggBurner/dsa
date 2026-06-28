6f6259e92c791c27aa303f2f49dd2b6085877918
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 13:20:13 +0500

    added brute force validAnagram

--- hashtable/validAnagram/brute/validAnagram.java
+++ hashtable/validAnagram/brute/validAnagram.java
@@ -1,5 +1,21 @@
 package hashtable.validAnagram.brute;
 
-public class validAnagram {
-    
+import java.util.Arrays;
+
+class validAnagram {
+    public static boolean validAnagramF(String s, String t) {
+
+        if (s.length() !=t.length()) {
+            return false;
+        }
+        char[] sarr = s.toCharArray();
+        char[] tarr = t.toCharArray();
+        Arrays.sort(sarr);
+        Arrays.sort(tarr);
+
+        return Arrays.equals(sarr, tarr);
+    }
+    public static void main(String[] args) {
+        System.out.println(validAnagramF("anagram", "nagaram"));
+    }
 }
