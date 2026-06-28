b62be99f99ed37fe2551a7ee95970226a284399c
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 16:31:25 +0500

    added optimal InvertBinaryTree

--- bst/invertBinaryTree/optimal/invertBinaryTree.java
+++ bst/invertBinaryTree/optimal/invertBinaryTree.java
@@ -1,5 +1,17 @@
 package bst.invertBinaryTree.optimal;
 
-public class invertBinaryTree {
-    
+import bst.TreeNode;
+
+class InvertBinaryTree {
+
+    public static TreeNode InvertBinaryTreeF(TreeNode root) {
+
+        if (root == null) return null;
+
+        TreeNode temp = root.left;
+        root.left = InvertBinaryTreeF(root.right);
+        root.right = InvertBinaryTreeF(temp);
+
+        return root;
+    }
 }
