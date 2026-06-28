6e9fa99fbb8acd90658333c642ff4d7c45da57e0
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 16:13:38 +0500

    added brute force topKFrequentElement

--- heap/topKFrequentElement/brute/topKFreuquentElement.java
+++ heap/topKFrequentElement/brute/topKFreuquentElement.java
@@ -1,5 +1,25 @@
 package heap.topKFrequentElement.brute;
 
-public class topKFreuquentElement {
-    
+import java.util.*;
+
+class TopKFrequent {
+    public static int[] TopKFrequentF(int[] nums, int k) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        for (int x : nums) {
+            freq.put(x, freq.getOrDefault(x, 0) + 1);
+        }
+
+        List<Integer> list = new ArrayList<>(freq.keySet());
+
+        list.sort((a, b) -> freq.get(b) - freq.get(a));
+
+        int[] res = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            res[i] = list.get(i);
+        }
+
+        return res;
+    }
 }
