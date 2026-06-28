872974fc5cfdb0e265d15342bb92d777ab0104e8
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 13:27:08 +0500

    added optimal validAnagram

--- hashtable/validAnagram/optimal/validAnagram.java
+++ hashtable/validAnagram/optimal/validAnagram.java
@@ -1,5 +1,23 @@
 package hashtable.validAnagram.optimal;
 
-public class validAnagram {
-    
+class validAnagram {
+    public static boolean validAnagramF(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] arr = new int[26];
+        
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) -'a']++;
+            arr[t.charAt(i) -'a']--;
+        }
+        for (int v: arr) {
+            if (v != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(validAnagramF("anagram", "nagaram"));
+    }
 }
