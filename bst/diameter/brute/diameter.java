f1b35ae18d3fa8c999e56ec7abb23d96743d227a
Abdullah Islam <itsabddev@gmail.com>
2026-06-28 16:36:26 +0500

    added brute force diameter

--- bst/diameter/brute/diameter.java
+++ bst/diameter/brute/diameter.java
@@ -1,5 +1,28 @@
 package bst.diameter.brute;
 
-public class diameter {
-    
+import bst.TreeNode;
+
+class DiameterOfBinaryTree {
+
+    public static int DiameterOfBinaryTreeF(TreeNode root) {
+
+        if (root == null) return 0;
+
+        int left = height(root.left);
+        int right = height(root.right);
+
+        int curr = left + right;
+
+        int leftDia = DiameterOfBinaryTreeF(root.left);
+        int rightDia = DiameterOfBinaryTreeF(root.right);
+
+        return Math.max(curr, Math.max(leftDia, rightDia));
+    }
+
+    private static int height(TreeNode root) {
+
+        if (root == null) return 0;
+
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
 }
